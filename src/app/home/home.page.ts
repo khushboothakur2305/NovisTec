d58619de6566a994da0fe3a3ff9a2c3089ad19e5
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-07-14 14:50:27 +0530

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+text="Change the text";
   constructor() {}
-
+  OnChangeText(){
+this.text="Text Is Changed"
+  }
 }
