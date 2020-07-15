2ed5e8730719f5c1e2c5dae47812b1326f400012
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-07-15 22:16:25 +0530

    update

--- src/app/select-club/select-club.page.ts
+++ src/app/select-club/select-club.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { club } from "./club.model";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-select-club",
@@ -9,15 +9,36 @@ import { Router } from '@angular/router';
 })
 export class SelectClubPage implements OnInit {
   club: club[] = [
-    { name: "Fiction Club", address: "Kimgston Road", image_url: "/assets/lovechildLogo.png" },
-    { name: "Neon Club", address: "828 South Ketch Harbour Lane", image_url: "/assets/lovechildLogo.png" },
-    { name: "Rico's Club", address: "115 Oak Drive", image_url: "/assets/lovechildLogo.png" },
-    { name: "Tamilarsi Club", address: "8103 Birchwood Street", image_url: "/assets/lovechildLogo.png" },
-    { name: "Kim-Yu Club", address: "8881 homestead St.", image_url: "/assets/lovechildLogo.png" },  ];
-  constructor(private router:Router) {}
+    {
+      name: "Fiction Club",
+      address: "Kimgston Road",
+      image_url: "/assets/lovechildLogo.png",
+    },
+    {
+      name: "Neon Club",
+      address: "828 South Ketch Harbour Lane",
+      image_url: "/assets/lovechildLogo.png",
+    },
+    {
+      name: "Rico's Club",
+      address: "115 Oak Drive",
+      image_url: "/assets/lovechildLogo.png",
+    },
+    {
+      name: "Tamilarsi Club",
+      address: "8103 Birchwood Street",
+      image_url: "/assets/lovechildLogo.png",
+    },
+    {
+      name: "Kim-Yu Club",
+      address: "8881 homestead St.",
+      image_url: "/assets/lovechildLogo.png",
+    },
+  ];
+  constructor(private router: Router) {}
 
   ngOnInit() {}
-  enter(clubname:string){
-this.router.navigate(['otp1']);
- }
+  enter(clubname: string) {
+    this.router.navigate(["otp1"]);
+  }
 }
