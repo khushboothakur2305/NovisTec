9dea98d9a15dcd860ccd9035ae2f095a3925da24
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-07-16 18:28:22 +0530

    update

--- src/app/select-city/select-city.page.ts
+++ src/app/select-city/select-city.page.ts
@@ -7,15 +7,53 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./select-city.page.scss"],
 })
 export class SelectCityPage implements OnInit {
-  city = [
-    "New york",
-    "Los Angeles",
-    "Toronto",
-    "London",
-    "Montreal",
-    "Vancouver",
-  ];
-  constructor() {}
+  Cities;
+
+  constructor() {
+    this.initializeCities();
+  }
+
+  initializeCities() {
+    this.Cities = [
+      "Amsterdam",
+      "Bogota",
+      "Buenos Aires",
+      "Cairo",
+      "Dhaka",
+      "Edinburgh",
+      "Geneva",
+      "Genoa",
+      "Glasglow",
+      "Hanoi",
+      "Hong Kong",
+      "Islamabad",
+      "Istanbul",
+      "Jakarta",
+      "Kiel",
+      "Kyoto",
+      "Le Havre",
+      "Lebanon",
+      "Lhasa",
+      "Lima",
+      "London",
+      "Los Angeles",
+      "Madrid",
+      "Manila",
+      "New York",
+      "Olympia",
+      "Oslo",
+      "Panama City",
+      "Peking",
+      "Philadelphia",
+      "San Francisco",
+      "Seoul",
+      "Taipeh",
+      "Tel Aviv",
+      "Tokio",
+      "Uelzen",
+      "Washington",
+    ];
+  }
 
   onsubmit(f: NgForm) {
     console.log(f.value.select);
--- src/app/select-club/select-club.page.ts
+++ src/app/select-club/select-club.page.ts
@@ -8,37 +8,52 @@ import { Router } from "@angular/router";
   styleUrls: ["./select-club.page.scss"],
 })
 export class SelectClubPage implements OnInit {
-  club: club[] = [
-    {
-      name: "Fiction Club",
-      address: "Kimgston Road",
-      image_url: "/assets/lovechildLogo.png",
-    },
-    {
-      name: "Neon Club",
-      address: "828 South Ketch Harbour Lane",
-      image_url: "/assets/lovechildLogo.png",
-    },
-    {
-      name: "Rico's Club",
-      address: "115 Oak Drive",
-      image_url: "/assets/lovechildLogo.png",
-    },
-    {
-      name: "Tamilarsi Club",
-      address: "8103 Birchwood Street",
-      image_url: "/assets/lovechildLogo.png",
-    },
-    {
-      name: "Kim-Yu Club",
-      address: "8881 homestead St.",
-      image_url: "/assets/lovechildLogo.png",
-    },
-  ];
-  constructor(private router: Router) {}
+  Clubs;
+
+  constructor(private router: Router) {
+    this.initializeClubs();
+  }
 
   ngOnInit() {}
   enter(clubname: string) {
     this.router.navigate(["otp1"]);
+  }
+  initializeClubs() {
+    this.Clubs = [
+      {
+        name: "Fiction Club",
+        address: "Kimgston Road",
+        image_url: "/assets/lovechildLogo.png",
+      },
+      {
+        name: "Neon Club",
+        address: "828 South Ketch Harbour Lane",
+        image_url: "/assets/lovechildLogo.png",
+      },
+      {
+        name: "Rico's Club",
+        address: "115 Oak Drive",
+        image_url: "/assets/lovechildLogo.png",
+      },
+      {
+        name: "Tamilarsi Club",
+        address: "8103 Birchwood Street",
+        image_url: "/assets/lovechildLogo.png",
+      },
+      {
+        name: "Kim-Yu Club",
+        address: "8881 homestead St.",
+        image_url: "/assets/lovechildLogo.png",
+      },
+    ];
+  }
+  getClubs(ev) {
+    this.initializeClubs();
+    var val = ev.target.value;
+    if (val && val.trim() != "") {
+      this.Clubs = this.Clubs.filter((Club) => {
+        return Club.name.toLowerCase().indexOf(val.toLowerCase()) > -1;
+      });
+    }
   }
 }
